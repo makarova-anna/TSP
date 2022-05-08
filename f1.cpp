44bebf57e9c4d7a31de9945719633af4de593774
makarova-anna <104019453+makarova-anna@users.noreply.github.com>
2022-05-08 21:00:58 +0300

    Update f1
    supplement for matrix class, operator < and the struct that'll contain the sollution

--- f1.cpp
+++ f1.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <iostream>
 #include <vector>
 #include <cmath>
+#include <cstdlib>
+#include <string>
+#include <fstream>
 using namespace std;
 
 double dist(double Ax, double Ay, double Bx, double By)
@@ -8,13 +12,15 @@ double dist(double Ax, double Ay, double Bx, double By)
     return sqrt(pow(Ax - Bx, 2) + pow(Ay - By, 2));
 }
 
+struct result;
+
 class matrix
 {
 private:
 	double** data;
 	int* Di;
 	int* Dj;
-	int D = 10000000;
+	unsigned sz;
 
 	void set_M(vector<pair<double, double>> D);
     void set_M(double** D, int n, int* di = NULL, int* dj = NULL);
@@ -23,13 +29,48 @@ private:
 public:
 	matrix();
 	matrix(const matrix &A);
-		matrix(vector<pair<double, double>> D);
+	matrix(vector<pair<double, double>> D);
     matrix(double** D, int n, int* di = NULL, int* dj = NULL);
 	~matrix();
+	double st1();
+	void set (int eI, int eJ, double S = -1);
+	double getResultSum(vector<pair<int, int>> result);
+	void clear();
+	bool is_empty() const;
+    unsigned size() const;
+	void erase(int I, int J);
+	double* operator[](int i) const;
+	static res Run(res A);
+    matrix& operator= (const matrix& D);
 
 };
+
+bool operator< (const res& A,const res& B)
+{
+	if (A.H == B.H)
+		return A.R.size() < B.R.size();
+	return A.H > B.H;
+}
+
+struct result
+{
+	double H;
+	vector <pair<int, int>> R;
+	matrix M;
+	res(double H1, vector <pair<int, int>> R1, matrix mf)
+	{
+		H = H1;
+		R = R1;
+		M = mf;
+	}
+
+
+	friend bool operator< (const res& A, const res& B);
+
+};
+
 int main()
 {
-    cout << "Hello world!" << endl;
+
     return 0;
 }
